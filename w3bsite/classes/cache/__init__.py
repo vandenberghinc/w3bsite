f44907eb67ce2a00c4e98572fde9536fb5d758a4
Daan van den Bergh <vandenberghinc.contact@gmail.com>
2021-03-04 14:10:52 +0100

    Updates
    updates.

--- w3bsite/classes/cache/__init__.py
+++ w3bsite/classes/cache/__init__.py
@@ -1,7 +1,7 @@
 # user wide cache to share variables accross tty sessions since default env variables is instable.
 import syst3m
 from fil3s import *
-if not Files.exists(f"{syst3m.default.vars.home}/.w3bsite/"): 
-	Files.create(path=f"{syst3m.default.vars.home}/.w3bsite/", directory=True)
+if not Files.exists(f"{syst3m.defaults.vars.home}/.w3bsite/"): 
+	Files.create(path=f"{syst3m.defaults.vars.home}/.w3bsite/", directory=True)
 cache = syst3m.cache.Cache(
-	path=f"{syst3m.default.vars.home}/.w3bsite/.cache/")
+	path=f"{syst3m.defaults.vars.home}/.w3bsite/.cache/")
