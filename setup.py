5f1fff541f9e6caca62829d94b472d0b01c26387
Daan van den Bergh <vandenberghinc.contact@gmail.com>
2021-03-09 11:46:34 +0100

    Updates
    updates.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages
 setup(
 	name='w3bsite',
-	version='4.21.1',
+	version='4.21.2',
 	description='Some description.',
 	url='http://github.com/vandenberghinc/w3bsite',
 	author='Daan van den Bergh',
@@ -35,8 +35,8 @@ setup(
             'django',
             'xmltodict',
             'stripe',
-            'syst3m>=2.16.7',
-            'dev0s>=2.16.4',
-            'netw0rk>=1.9.3',
-            'ssht00ls>=3.20.8',
+            #'syst3m>=2.16.7',
+            #'dev0s>=2.16.4',
+            #'netw0rk>=1.9.3',
+            #'ssht00ls>=3.20.8',
         ])
