bdd286102dd087e19e832784a029a5c315ea8e36
Daan van den Bergh <vandenberghinc.contact@gmail.com>
2021-03-10 23:11:27 +0100

    Updates
    updates.

--- w3bsite/classes/users/__init__.py
+++ w3bsite/classes/users/__init__.py
@@ -3,15 +3,10 @@
 
 # imports.
 from w3bsite.classes.config import *
-from django.contrib.auth.models import User as DjangoUser
 if not Environment.get("MIGRATIONS", format=bool, default=False):
-	try:
-		#from django.contrib.auth.models import User as DjangoUser
-		from django.contrib.auth import authenticate, login
-		from django.contrib.auth import login as _login_
-	except Exception as e:
-		if Environment.get_boolean("DJANGO_RUNNING") == True:
-			raise ValueError(e)
+	from django.contrib.auth.models import User as DjangoUser
+	from django.contrib.auth import authenticate, login
+	from django.contrib.auth import login as _login_
 try:
 	from w3bsite.classes.firebase import *
 	from firebase_admin import credentials, auth, firestore, _auth_utils
