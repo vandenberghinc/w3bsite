92fb816f2f23a40c81ed13ab6a5bdd8668e2d09d
Daan van den Bergh <vandenberghinc.contact@gmail.com>
2021-03-11 09:27:44 +0100

    Updates
    updates.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages
 setup(
 	name='w3bsite',
-	version='4.32.4',
+	version='4.32.5',
 	description='Some description.',
 	url='http://github.com/vandenberghinc/w3bsite',
 	author='Daan van den Bergh',
